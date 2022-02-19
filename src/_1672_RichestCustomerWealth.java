9c5354b933fc292a1e5361f30309d2052c8082a5
Nguyen Xuan Anh <fr13nd94@gmail.com>
2022-02-19 21:53:06 +0700

    1672 Ok

---
+++ src/_1672_RichestCustomerWealth.java
@@ -0,0 +1,21 @@
+public class _1672_RichestCustomerWealth {
+    public static int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int i = 0; i < accounts.length; i++){
+            int check = 0;
+            for (int a: accounts[i]){
+                check += a;
+            }
+            if (check > max){
+                max = check;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] accounts = {{1,2,4}, {3,2,1}};
+        int max = maximumWealth(accounts);
+        System.out.println(max);
+    }
+}
--- src/_88_MergeSorted.java
+++ src/_88_MergeSorted.java
@@ -1,5 +1,5 @@
 public class _88_MergeSorted {
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
         for (int ai: nums2){
             insertElement(ai, nums1, m);
             m++;
@@ -22,10 +22,35 @@ public class _88_MergeSorted {
             nums1[m] = ai;
         }
     }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = ( m + n ) - 1;
+        while (k >= 0){
+            if (i < 0){
+                nums1[k] = nums2[j];
+                j--;
+            }
+            else if (j < 0){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else if (nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0, 0, 0};
-        int[] nums2 = {1,2,3};
+        int[] nums2 = {1,2,4};
         merge(nums1, 3, nums2, 3);
         for (int ai: nums1){
             System.out.println(ai);
