a516148206f12410fd5eca67be81dd42249370e5
Nguyen Xuan Anh <fr13nd94@gmail.com>
2022-02-19 22:18:45 +0700

    387 First Unique Character in a String OK

---
+++ src/_387FirstUniqueCharacterinaString.java
@@ -0,0 +1,24 @@
+public class _387FirstUniqueCharacterinaString {
+    public static int firstUniqChar(String s) {
+        char[] arr = s.toCharArray();
+        int[] count = new int[123];
+        for (int i = 0; i < arr.length; i++){
+            char c = arr[i];
+            int index = (int) c;
+            count[index]++;
+        }
+        for (int i = 0; i < arr.length; i++){
+            char c = arr[i];
+            int index = (int) c;
+            if (count[index] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "aabb";
+        System.out.println(firstUniqChar(s));
+    }
+}
