e58bc74d800d6582ed83055671f061f08eca204f
Nguyen Xuan Anh <fr13nd94@gmail.com>
2022-02-20 17:11:58 +0700

    746 Min Cost Climbing Stairs OK

---
+++ src/_746MinCostClimbingStairs.java
@@ -0,0 +1,21 @@
+public class _746MinCostClimbingStairs {
+    public static int minCostClimbingStairs(int[] cost) {
+        int[] money = new int[cost.length+1];
+        money[0] = 0;
+        money[1] = 0;
+        for (int i = 2; i < cost.length+1; i++){
+            if ((money[i-1] + cost[i-1]) < (money[i-2] + cost[i-2])){
+                money[i] = money[i-1] + cost[i-1];
+            }
+            else {
+                money[i] = money[i -2] + cost[i-2];
+            }
+        }
+        return money[cost.length];
+    }
+
+    public static void main(String[] args) {
+        int[] cost = {10,15,20};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+}
