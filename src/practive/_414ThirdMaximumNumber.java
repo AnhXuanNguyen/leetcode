02a8e39b2badbd664d1943275def16ba755b010d
Nguyen Xuan Anh <fr13nd94@gmail.com>
2022-02-25 16:38:51 +0700

    414ThirdMaximumNumber

---
+++ src/practive/_414ThirdMaximumNumber.java
@@ -0,0 +1,39 @@
+package practive;
+
+public class _414ThirdMaximumNumber {
+    public static int thirdMax(int[] nums){
+        long[] maxValue = {Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE};
+        for (int val: nums){
+            insert(val, maxValue);
+        }
+        if (maxValue[2] != Long.MIN_VALUE){
+            return (int) maxValue[2];
+        }
+        return (int) maxValue[0];
+    }
+
+    private static void insert(int val, long[] maxValue) {
+        int i = 0;
+        while (i < maxValue.length){
+            if (maxValue[i] == val){
+                return;
+            }
+            if (maxValue[i] < val){
+                break;
+            }
+            else {
+                i++;
+            }
+        }
+        if (i < maxValue.length){
+            for (int j = maxValue.length-2; j >= i; j--){
+                maxValue[j+1] = maxValue[j];
+            }
+            maxValue[i] = val;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {2,2,3,5};
+        System.out.println(thirdMax(array));
+    }
+}
