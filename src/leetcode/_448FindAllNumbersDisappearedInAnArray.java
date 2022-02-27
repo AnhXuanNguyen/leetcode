33361514103f90e11508a20340f54ec6712f00a7
Nguyen Xuan Anh <fr13nd94@gmail.com>
2022-02-27 21:32:27 +0700

    448 OK

--- src/leetcode/_448FindAllNumbersDisappearedInAnArray.java
+++ src/leetcode/_448FindAllNumbersDisappearedInAnArray.java
@@ -7,7 +7,7 @@ public class _448FindAllNumbersDisappearedInAnArray {
     public static List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < nums.length; i++){
-            nums[Math.abs(nums[i]) - 1] = nums[nums[i] - 1] * -1;
+            nums[Math.abs(nums[i]) - 1] = Math.abs(nums[Math.abs(nums[i]) - 1]) * -1;
         }
         for (int i = 0; i < nums.length; i++){
             if (nums[i] > 0){
@@ -18,7 +18,7 @@ public class _448FindAllNumbersDisappearedInAnArray {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,1};
+        int[] nums = {4,3,2,7,8,2,3,1};
         List<Integer> result = findDisappearedNumbers(nums);
         for (Integer num : result){
             System.out.println(num);
